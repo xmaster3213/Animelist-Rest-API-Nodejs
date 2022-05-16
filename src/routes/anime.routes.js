0cf3eec8e7ac9c93273fe8752667b2d6d076928a
xmaster3213 <emailperlascuolamia@gmail.com>
2022-05-16 11:36:10 +0200

    Added comments for routes

--- src/routes/anime.routes.js
+++ src/routes/anime.routes.js
@@ -2,9 +2,13 @@ module.exports = app => {
     const anime = require("../controllers/anime.controller.js");
     const auth = require("../middleware/auth.js");
     var router = require("express").Router();
+    
     // Create a new Anime
+    // Need authentication
     router.post("/", auth, anime.create);
+    // Get all anime
     router.get("/", anime.getAll);
+    // Get an anime by his id
     router.get("/:id", anime.findById);
 
     app.use('/api/anime', router);
