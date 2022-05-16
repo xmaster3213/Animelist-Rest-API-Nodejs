bcf5129b0f88a65c68c1265854d431890eee40bf
xmaster3213 <emailperlascuolamia@gmail.com>
2022-05-16 11:43:17 +0200

    Removed debugging logs

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -11,7 +11,6 @@ const verifyToken = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, config.TOKEN_KEY);
     req.user = decoded;
-    console.log(req);
   } catch (err) {
     return res.status(401).send("Invalid Token");
   }
